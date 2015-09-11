5074a7b5c33f8ed66010d7691cae70099ee2b098
mcclure15 <m001010111@gmail.com>
2015-09-11 11:59:26 -0700

    add room list vc

---
+++ GrandHotel/RoomListViewController.h
@@ -0,0 +1,16 @@
+//
+//  RoomListViewController.h
+//  GrandHotel
+//
+//  Created by Matthew McClure on 9/11/15.
+//  Copyright (c) 2015 Matthew McClure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoomListViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property(strong, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) NSSet *rooms;
+
+@end
--- GrandHotel/RoomListViewController.m
+++ GrandHotel/RoomListViewController.m
@@ -1,24 +1,22 @@
 //
-//  HotelListViewController.m
+//  RoomListViewController.m
 //  GrandHotel
 //
-//  Created by Matthew McClure on 9/7/15.
+//  Created by Matthew McClure on 9/11/15.
 //  Copyright (c) 2015 Matthew McClure. All rights reserved.
 //
 
-#import "HotelListViewController.h"
 #import "RoomListViewController.h"
+#import "HotelListViewController.h"
 #import "AppDelegate.h"
 #import "Hotel.h"
 
-@interface HotelListViewController ()
 
-//@property NSArray *hotels;
+@interface RoomListViewController ()
 
 @end
 
-@implementation HotelListViewController
-
+@implementation RoomListViewController
 
 -(void)loadView {
   UIView *rootView = [[UIView alloc] init];
@@ -40,37 +38,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-  NSLog(@"view did loaded");
   self.tableView.delegate = self;
   self.tableView.dataSource = self;
-  
-  
-  AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
-  NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Hotel"];
-  
-  //  fetchRequest.predicate = [NSPredicate predicateWithFormat:@"name MATCHES %@",@"Four Seasons"];
-  
-  //fetchRequest.predicate  = [NSPredicate predicateWithFormat:@"stars < 3"];
-  
-  NSError *fetchError;
-  self.hotels = [appDelegate.managedObjectContext executeFetchRequest:fetchRequest error:&fetchError];
-  
-  //NSLog(@"hotel count: %lu", (unsigned long)self.hotels.count);
-  
-//  for (int i = 0; i < self.hotels.count; i++){
-//    
-//    NSLog(@"%@", self.hotels[i]);
-//    
-//  }
-//  
-  
-  
-  
-  if (fetchError) {
-    NSLog(@"Here is the fetchError: %@",fetchError.localizedDescription);
-  }
-  [self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -81,11 +50,11 @@
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  return [self.hotels count];
+  return self.rooms.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-  NSString *cellIdentifier = @"hotelCell";
+  NSString *cellIdentifier = @"roomCell";
   UITableViewCell *cell = (UITableViewCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
   
   if (cell == nil)
@@ -95,9 +64,12 @@
   
   cell.backgroundColor = [UIColor colorWithRed:249.0/255 green:237.0/255 blue:224.0/255 alpha:1.0];
   
-  int dataIndex = (int) indexPath.row % [self.hotels count];
-  NSString *hotelName = [self.hotels[dataIndex] name];
-  cell.textLabel.text = hotelName;
+  int dataIndex = (int) indexPath.row % [self.rooms count];
+//  //NSNumber *hotelName = [self.rooms[dataIndex] number];
+//  Room *roomForRow = [self.rooms[dataIndex]];
+//  NSNumber *roomNum = [self.rooms[dataIndex] number];
+//  cell.textLabel.text = hotelName;
+  cell.textLabel.text = @"\(dataIndex)";
   cell.textLabel.numberOfLines = 0;
   cell.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
   
@@ -118,15 +90,7 @@
   
 }
 
-#pragma mark - UITableViewDelegate
 
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-  Hotel *selectedHotel = self.hotels[indexPath.row];
-  NSLog(@"*****%@", [selectedHotel rooms]);
-  
-  RoomListViewController *roomListVC = [[RoomListViewController alloc] init];
-  roomListVC.rooms = [selectedHotel rooms];
-  [self.navigationController pushViewController:roomListVC animated:YES];
-}
+
 
 @end
